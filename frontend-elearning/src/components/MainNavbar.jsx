2c9655162d9d8256baac2616cafce68f89805ebe
EzechielKiregha <ekiregha@gmail.com>
2024-06-26 21:08:30 +0200

    logo Update

--- frontend-elearning/src/components/MainNavbar.jsx
+++ frontend-elearning/src/components/MainNavbar.jsx
@@ -1,6 +1,6 @@
 import { Menu, X } from 'lucide-react'
 import { useState} from 'react'
-import logo from '../assets/log1.png'
+import logo from '../assets/logo.png'
 import { navItems } from '../constants'
 
 const MainNavbar = () => {
